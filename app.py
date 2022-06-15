7e8ed0495b2386a03f1b58ca5f6e67a5c5bdfe2c
amitavitan <avamit@post.bgu.ac.il>
2022-06-16 01:39:33 +0300

    first

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
-import streamlit as st  # pip install streamlit
+import streamlit as st  # pip install streamli
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
